ffb8374772a999ecbe61f7a1a10281b883e71f03
edvin <edvinkeskin@gmail.com>
2022-08-08 12:57:31 +0300

    save

---
+++ mern/README.md
@@ -0,0 +1,5 @@
+setup:
+
+go to server folder run 'node server.js'
+
+go to client folder npm start
--- mern/client/src/components/LeftSplit/LeftSplit.jsx
+++ mern/client/src/components/LeftSplit/LeftSplit.jsx
@@ -2,6 +2,50 @@ import './LeftSplit.css'
 import {useState} from "react";
 import {createDocument} from 'backend'
 
+// create
+async function onSubmit(e) {
+    e.preventDefault();
+
+    // When a post request is sent to the create url, we'll add a new record to the database.
+    const newPerson = { ...form };
+
+    await fetch("http://localhost:5000/record/add", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(newPerson),
+    })
+        .catch(error => {
+            window.alert(error);
+            return;
+        });
+
+    setForm({ name: "", position: "", level: "" });
+    navigate("/");
+}
+
+
+// edit
+async function onSubmit(e) {
+    e.preventDefault();
+    const editedPerson = {
+        name: form.name,
+        position: form.position,
+        level: form.level,
+    };
+
+    // This will send a post request to update the data in the database.
+    await fetch(`http://localhost:5000/update/${params.id}`, {
+        method: "POST",
+        body: JSON.stringify(editedPerson),
+        headers: {
+            'Content-Type': 'application/json'
+        },
+    });
+
+    navigate("/");
+}
 
 
 const LeftSplit = (props) => {
